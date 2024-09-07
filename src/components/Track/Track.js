9877919abbf084ec4786dadb9a84c57e1b0c6564
amryoussef92 <amryusif@gmail.com>
2024-09-07 14:08:34 +0300

    track

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -31,12 +31,12 @@ class Track extends React.Component {
   render() {
     return (
       <div className="Track">
-        <div className="Track-information">
+        {/* <div className="Track-information">
           <h3>{this.props.track.name}</h3>
           <p>
             {this.props.track.artist} | {this.props.track.album}
           </p>
-        </div>
+        </div> */}
         {this.renderAction()}
         <iframe
           src={`https://open.spotify.com/embed/track/${this.props.track.id}`}
